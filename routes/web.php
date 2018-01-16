a7806e0e3acc845eff43873242d3511f7d6c1d18
Milos Djokic - ztech <mdjokic@ztech.io>
2018-01-16 15:13:26 +0100

    admin routes middleware added

--- routes/web.php
+++ routes/web.php
@@ -31,42 +31,47 @@ Route::group(['prefix' => env('ADMIN_PANEL_LOCATION', 'admin')], function () {
 	Route::get('/password/reset', 'AdminAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
 	Route::get('/password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm');
 
-	// admin panel dashboard page
-	Route::get('/dashboard', 'Admin\DashboardController@index')->name('admin.dashboard');
+	/**
+	 * restricted for logged-in admin user
+	 */
 
-	// user pages
-	Route::get('/users/users-all', 'Admin\UsersController@index')->name('admin.users.users_all.list');
-	Route::get('/users/advocates', 'Admin\UsersController@advocates')->name('admin.users.advocates.list');
-	Route::get('/users/shelters', 'Admin\UsersController@shelters')->name('admin.users.shelters.list');
-	Route::get('/users/user-add', 'Admin\UsersController@add')->name('admin.users.user_add.list');
+	Route::group(['middleware' => ['admin']], function () {
 
-	// client pages
-	Route::get('/clients/clients-all', 'Admin\ClientsController@index')->name('admin.clients.clients_all.list');
-	Route::get('/clients/client-add', 'Admin\ClientsController@add')->name('admin.clients.client_add.list');
-	
-	// pet pages
-	Route::get('/clients/pets-all', 'Admin\PetsController@index')->name('admin.clients.pets_all.list');
-	
-	// client applications pages
-	Route::get('/clients/applications', 'Admin\ApplicationsController@index')->name('admin.clients.applications.list');
-	
-	// settings pages
-	Route::get('/settings/application', 'Admin\ApplicationSettingsController@index')->name('admin.settings.application');
-	Route::get('/settings/admin-users', 'Admin\AdminUsersController@index')->name('admin.settings.admin-users');
-	Route::get('/settings/account', 'Admin\AccountSettingsController@index')->name('admin.settings.account');
+		// admin panel dashboard page
+		Route::get('/dashboard', 'Admin\DashboardController@index')->name('admin.dashboard');
+
+		// user pages
+		Route::get('/users/users-all', 'Admin\UsersController@index')->name('admin.users.users_all.list');
+		Route::get('/users/advocates', 'Admin\UsersController@advocates')->name('admin.users.advocates.list');
+		Route::get('/users/shelters', 'Admin\UsersController@shelters')->name('admin.users.shelters.list');
+		Route::get('/users/user-add', 'Admin\UsersController@add')->name('admin.users.user_add.list');
+
+		// client pages
+		Route::get('/clients/clients-all', 'Admin\ClientsController@index')->name('admin.clients.clients_all.list');
+		Route::get('/clients/client-add', 'Admin\ClientsController@add')->name('admin.clients.client_add.list');
+		
+		// pet pages
+		Route::get('/clients/pets-all', 'Admin\PetsController@index')->name('admin.clients.pets_all.list');
+		
+		// client applications pages
+		Route::get('/clients/applications', 'Admin\ApplicationsController@index')->name('admin.clients.applications.list');
+		
+		// settings pages
+		Route::get('/settings/application', 'Admin\ApplicationSettingsController@index')->name('admin.settings.application');
+		Route::get('/settings/admin-users', 'Admin\AdminUsersController@index')->name('admin.settings.admin-users');
+		Route::get('/settings/account', 'Admin\AccountSettingsController@index')->name('admin.settings.account');
+
+	});
 
 });
 
-
-// Route::group(['middleware' => 'web'], function () {
-
+	/**
+	 * user auth routes
+	 */
 	Auth::routes();
 
-	// Route::get('/home', 'HomeController@index')->name('home');
 
 	/**
 	 *  user dashboard page
 	 */
 	Route::get('/dashboard', 'HomeController@index')->name('user.dashboard');
-
-// });
