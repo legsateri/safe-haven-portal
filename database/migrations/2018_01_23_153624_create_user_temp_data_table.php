e239af9f3bef3b19d3a4b0915595da8becb2641b
Milos Djokic - ztech <mdjokic@ztech.io>
2018-01-24 12:19:10 +0100

    class name fix

--- database/migrations/2018_01_23_153624_create_user_temp_data_table.php
+++ database/migrations/2018_01_23_153624_create_user_temp_data_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserTempDatasTable extends Migration
+class CreateUserTempDataTable extends Migration
 {
     /**
      * Run the migrations.
