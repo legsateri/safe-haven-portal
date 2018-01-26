974a2f6870a2b8f38d5d3ad8c4b91a978ba85f0c
milicadundic <mdundic@ztech.io>
2018-01-26 16:26:59 +0100

    Admin account settings page

--- app/Http/Controllers/Admin/AccountSettingsController.php
+++ app/Http/Controllers/Admin/AccountSettingsController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Auth;
+use Hash;
+use Validator;
+use App\Admin;
+
 class AccountSettingsController extends Controller
 {
     /**
@@ -12,7 +17,97 @@ class AccountSettingsController extends Controller
      * for current logged-in admin user
      */
     public function index()
+    {        
+        $admin['name'] = Auth('admin')->user()->name;
+        $admin['email'] = Auth('admin')->user()->email;
+        // dd($admin);
+        return view('admin.settings.account', compact('admin'));
+    }
+
+    function updateInfo(Request $request)
     {
-        return view('admin.settings.account');
+        if(isset(Auth('admin')->user()->id))                        
+        {
+            //validate data from form
+            $validator = Validator::make($request->all(),[          
+            'name'  =>  'required|max:255',                         
+            'email' =>  'required|email|max:255',
+            ]);
+
+            if (!($validator->fails()))                               
+            {
+                //find user's account
+                $admin = Admin::where('id', Auth('admin')->user()->id)->first();
+                
+                //check if email from request allready exists for some other admin/user
+                $check_admin = Admin::where('email', $request->email)->first();
+                
+                if($check_admin == null || ($check_admin != null && $check_admin->id == $admin->id)) 
+                {
+                    //update admin info: name and email
+                    $admin->name = $request->name;
+                    $admin->email = $request->email;
+                    $admin->update();
+                    return redirect()->back()->with('message0', 'Information successfully updated!');
+
+                } elseif ($check_admin->id != $admin->id) {
+                    return redirect()->back()->with('message', 'Admin user with this email address already exists!');
+                }
+            }
+        }
+    }    
+
+    public function updatePassword(Request $request)
+    {
+        if(isset(Auth('admin')->user()->id))
+        {
+            // validate data from from
+            $validator = Validator::make($request->all(), [
+                'old_password'  =>  'required|string|min:6|max:40',
+                'new_password'  =>  'required|string|min:6|max:40',
+                'new_password2' =>  'required|same:new_password',
+            ]);
+
+            if (!($validator->fails()))
+            {
+                // find user's account
+                $check_id = Auth('admin')->user()->id;
+                $admin = Admin::where('id', $check_id)->first();
+
+                // check if old password is valid
+                if (Hash::check($request->old_password, $admin->password))
+                {                  
+                    // update database
+                    $hashed = Hash::make($request->new_password);
+                    $admin->password = $hashed;
+                    $admin->update();
+                    // $admin_message = '<div class="alerts alert-green center"><p>Password updated!</p></div>';
+                    return redirect()->back()->with('message1', 'Password updated!');
+
+                } else {
+                    // invalid old password
+                    return redirect()->back()->with('message2', 'Invalid old password!');
+                    // $admin_message = '<div class="alerts alert-error center"><p>Invalid old password!</p></div>';
+                }
+                
+            } 
+            else 
+            {
+                // invalid entries in password fields
+                $errors = $validator->errors();
+
+                if ( $errors->first('old_password') != null ){
+                    return redirect()->back()->with('message3', $errors->first('old_password'));
+                }
+
+                if ( $errors->first('new_password') != null ){
+                    return redirect()->back()->with('message4', $errors->first('new_password'));
+                }
+
+                if ( $errors->first('new_password2') != null ){
+                    return redirect()->back()->with('message5', $errors->first('new_password2'));
+                }
+            }
+        }
     }
 }
--- resources/views/admin/settings/account.blade.php
+++ resources/views/admin/settings/account.blade.php
@@ -9,28 +9,70 @@
         </div>
         <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
     </div>
-    <div class="row">
-        <div class="col-lg-8">
-            <!-- Example Bar Chart Card-->
-            <div class="card mb-3">
-                <div class="card-header">
-                    <i class="fa fa-bar-chart"></i> Bar Chart Example</div>
-                <div class="card-body">
-                    <canvas id="myBarChart" width="100" height="50"></canvas>
-                </div>
-                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+
+        <div class="row">
+            <div class="col-lg-5">
+                <form method="post" action="{{ route('admin.settings.account.update.info') }}">
+                    {{ csrf_field() }}
+
+                    @if (session('message0'))
+                        {{ session('message0') }}
+                    @endif
+
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your username" value="{{ $admin['name'] }}">
+                    </div>     
+                    <div class="form-group">
+                        <label for="user_email">Email address</label>
+                        <input type="email" class="form-control" id="user_email" name="email" aria-describedby="emailHelp" placeholder="Enter your email" value="{{ $admin['email'] }}">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        @if (session('message'))
+                        {{ session('message') }}
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary mb-2">Update information</button>
+                    </div>
+                </form>
+            </div>
+            
+            
+            <div class="col-lg-5">
+                <form method="post" action="{{ route('admin.settings.account.update.password') }}">
+                    {{ csrf_field() }}
+                    
+                    @if (session('message1'))
+                        {{ session('message1') }}
+                    @endif                
+                    
+                    <div class="form-group">
+                        <label for="old_pass">Old password</label>
+                        <input type="password" class="form-control" id="old_pass" name="old_password" aria-describedby="passHelp" placeholder="Enter old password">
+                        @if (session('message2'))
+                        {{ session('message2') }}
+                        @elseif (session('message3'))
+                        {{ session('message3') }}
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="new_pass">New password</label>
+                        <input type="password" class="form-control" id="new_pass" name="new_password" placeholder="Enter new password">
+                        @if (session('message4'))
+                        {{ session('message4') }}
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="new_pass2" class="sr-only">New Password</label>
+                        <input type="password" class="form-control" id="new_pass2" name="new_password2" placeholder="Repeat new password">
+                        @if (session('message5'))
+                        {{ session('message5') }}
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary mb-2">Change password</button>
+                    </div>
+                </form>
             </div>
         </div>
-        <div class="col-lg-4">
-            <!-- Example Pie Chart Card-->
-            <div class="card mb-3">
-                <div class="card-header">
-                    <i class="fa fa-pie-chart"></i> Pie Chart Example</div>
-                <div class="card-body">
-                    <canvas id="myPieChart" width="100%" height="100"></canvas>
-                </div>
-                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-            </div>
-        </div>
-    </div>
 @endsection
