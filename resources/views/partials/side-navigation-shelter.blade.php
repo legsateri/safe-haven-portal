466f18b9d037ac42fcac2debe3284097ff0ab164
Milos Djokic - ztech <mdjokic@ztech.io>
2018-02-06 15:36:04 +0100

    sidebar dashboard link fixed

--- resources/views/partials/side-navigation-shelter.blade.php
+++ resources/views/partials/side-navigation-shelter.blade.php
@@ -1,5 +1,10 @@
 <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+          <a class="nav-link" href="{{ route('user.dashboard') }}">
+            <i class="fa fa-fw fa-dashboard"></i>
+            <span class="nav-link-text">Dashboard</span>
+          </a>
+        </li>
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
           <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapsePets" data-parent="#exampleAccordion">
             <i class="fa fa-paw" aria-hidden="true"></i>
