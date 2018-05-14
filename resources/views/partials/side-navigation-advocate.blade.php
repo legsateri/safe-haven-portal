ff7717e0219fd1c24f70c61e1f6e68949bea5da6
nstojcic <nstojcic@yahoo.com>
2018-05-14 15:15:10 +0200

    correct tooltips on sidebar icons

--- resources/views/partials/side-navigation-advocate.blade.php
+++ resources/views/partials/side-navigation-advocate.blade.php
@@ -5,16 +5,16 @@
             <img src="{{url('/')}}/img/SHN-side-logo-3-small.png" class="logo logo_collapsed_visible"/>
         </a>
     </li>
-    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+    <li class="nav-item" data-toggle="tooltip" data-placement="right">
       <a class="nav-link" href="{{ route('user.dashboard') }}">
-        <i class="fa fa-fw fa-dashboard"></i>
+        <i class="fa fa-fw fa-dashboard" title="Dashboard"></i>
         <span class="nav-link-text">Dashboard</span>
       </a>
     </li>
 
-    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Clients">
+    <li class="nav-item" data-toggle="tooltip" data-placement="right">
       <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseClients" data-parent="#exampleAccordion">
-        <i class="fa fa-user" aria-hidden="true"></i>
+        <i class="fa fa-user" aria-hidden="true" title="Clients"></i>
         <span class="nav-link-text">Clients</span>
       </a>
       <ul class="sidenav-second-level collapse" id="collapseClients">
@@ -32,9 +32,9 @@
       </ul>
     </li>
 
-    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="My Settings">
+    <li class="nav-item" data-toggle="tooltip" data-placement="right">
       <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseSettings" data-parent="#exampleAccordion">
-        <i class="fa fa-sliders" aria-hidden="true"></i>
+        <i class="fa fa-sliders" aria-hidden="true" title="My Settings"></i>
         <span class="nav-link-text"> Settings</span>
       </a>
       <ul class="sidenav-second-level collapse" id="collapseSettings">
@@ -48,15 +48,15 @@
         </li>
       </ul>
     </li>
-    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Contact SHN">
+    <li class="nav-item" data-toggle="tooltip" data-placement="right">
         <a href="mailto:webmaster@example.com" class="nav-link" >
-            <i class="fa fa-envelope" aria-hidden="true"></i>
+            <i class="fa fa-envelope" aria-hidden="true" title="Contact SHN"></i>
             <span class="nav-link-text"> Contact SHN</span>
         </a>
     </li>
-    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Logout">
+    <li class="nav-item" data-toggle="tooltip" data-placement="right">
         <a class="nav-link" data-toggle="modal" data-target="#logOutModal">
-            <i class="fa fa-fw fa-sign-out"></i><span class="nav-link-text">Logout</span></a>
+            <i class="fa fa-fw fa-sign-out" title="Logout"></i><span class="nav-link-text">Logout</span></a>
     </li>
 </ul>
 <ul class="navbar-nav sidenav-toggler">
--- resources/views/partials/side-navigation-shelter.blade.php
+++ resources/views/partials/side-navigation-shelter.blade.php
@@ -5,15 +5,15 @@
              <img src="{{url('/')}}/img/SHN-side-logo-3-small.png" class="logo logo_collapsed_visible"/>
          </a>
         </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right">
           <a class="nav-link" href="{{ route('user.dashboard') }}">
-            <i class="fa fa-fw fa-dashboard"></i>
+            <i class="fa fa-fw fa-dashboard" title="Dashboard"></i>
             <span class="nav-link-text">Dashboard</span>
           </a>
         </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right">
           <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapsePets" data-parent="#exampleAccordion">
-            <i class="fa fa-paw" aria-hidden="true"></i>
+            <i class="fa fa-paw" aria-hidden="true" title="Pets"></i>
             <span class="nav-link-text">Pets</span>
           </a>
           <ul class="sidenav-second-level collapse" id="collapsePets">
@@ -28,9 +28,9 @@
           </ul>
         </li>
 
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="My Settings">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right">
           <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseSettings" data-parent="#exampleAccordion">
-            <i class="fa fa-sliders" aria-hidden="true"></i>
+            <i class="fa fa-sliders" aria-hidden="true" title="My Settings"></i>
             <span class="nav-link-text"> My Settings</span>
           </a>
           <ul class="sidenav-second-level collapse" id="collapseSettings">
@@ -44,15 +44,15 @@
             </li>
           </ul>
         </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Contact SHN">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right">
             <a href="mailto:webmaster@example.com" class="nav-link" >
-                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <i class="fa fa-envelope" aria-hidden="true" title="Contact SHN"></i>
                 <span class="nav-link-text"> Contact SHN</span>
             </a>
         </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Logout">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right">
             <a class="nav-link" data-toggle="modal" data-target="#logOutModal">
-                <i class="fa fa-fw fa-sign-out"></i><span class="nav-link-text">Logout</span></a>
+                <i class="fa fa-fw fa-sign-out" title="Logout"></i><span class="nav-link-text">Logout</span></a>
         </li>
       </ul>
       <ul class="navbar-nav sidenav-toggler">
